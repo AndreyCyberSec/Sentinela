078a01ed3116fe9912547463eb9445ab2ab70b14
Andrey-dev-hub <andreymoreira138@gmail.com>
2026-08-05 20:58:08 -0300

    Subindo o Sentinela para o github

---
+++ Core/Models/LogEntity.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Models
+{
+    public record class LogEntity
+    {
+        public string IpAddress { get;}
+        public string Date { get; }
+        public string Endpoint { get; }
+        public string EndpointMethod { get; }
+        public int EndpointStatusCode { get; }
+        public string UserAgent { get; }
+
+        public LogEntity() { }
+        public LogEntity(string ipAddress, string date, string endpoint, string endpointMethod, int endpointStatusCode, string userAgent)
+        {
+            IpAddress = ipAddress;
+            Date = date;
+            Endpoint = endpoint;
+            EndpointMethod = endpointMethod;
+            EndpointStatusCode = endpointStatusCode;
+            UserAgent = userAgent;
+        }
+
+        public LogEntity(string ipAddress, string date)
+        {
+            IpAddress = ipAddress;
+            Date = date;
+        }
+    }
+}
---
+++ Sentinela/Scanner.cs
@@ -0,0 +1,107 @@
+﻿using Application.Service;
+using Spectre.Console;
+using System.Runtime.CompilerServices;
+
+namespace Sentinela
+{
+    public class Scanner
+    {
+        private static readonly IpAddressService addressService = new IpAddressService();
+        public static async Task Main(string[] args)
+        {
+            var title = new FigletText("Sentinela")
+      .Color(Color.Green);
+
+            var subtitle = new Markup("[italic grey]Welcome the tools of Sentinela[/]");
+            var subtitle1 = new Markup("[italic grey]Version: 1.0.0[/]");
+            var subtitle2 = new Markup("[italic grey]Development by Andrey[/]");
+
+            var titleAligned = Align.Center(title);
+            var subtitleAligned = Align.Left(subtitle);
+            var subtitle1Aligned = Align.Left(subtitle1);
+            var subtitle2Aligned = Align.Left(subtitle2);
+
+            AnsiConsole.Write(titleAligned);
+            AnsiConsole.Write(subtitleAligned);
+            AnsiConsole.Write(subtitle1Aligned);
+            AnsiConsole.Write(subtitle2Aligned);
+            AnsiConsole.WriteLine();
+            await AnsiConsole.Status()
+                      .Spinner(Spinner.Known.Binary)
+                     .StartAsync("Loading...", async ctx =>
+                          {
+                             await Task.Delay(1000);
+                          });
+
+            var toppings = AnsiConsole.Prompt(
+                     new MultiSelectionPrompt<string>()
+                        .Title("Choose the [green]tool[/]...")
+                        .NotRequired()
+                        .InstructionsText("[grey](Press [blue]<space>[/] to toggle, [green]<enter>[/] to confirm)[/]")
+                        .AddChoices("Sentinela-Analyzer-Log", "Management-.Env", "Net-WatchService",
+                                    "Register-Dns", "Sys-Auditor", "Check-SSl/TLS","[red]To finish[/]"));
+            while ( toppings.Last() != "[red]To finish[/]")
+            {
+                switch (toppings)
+                {
+                    case var t when t.Contains("Sentinela-Analyzer-Log"):
+                        AnsiConsole.MarkupLine("[green]You selected Sentinela-Analyzer-Log\nThis tool select the top 10 IP with date[/]");
+                        var confirmed = AnsiConsole.Confirm("Do you want to continue?");
+                        if (!confirmed)
+                        {
+                            AnsiConsole.MarkupLine("[red]Operation canceled by the user.[/]");
+                            break;
+                        }
+                        await AnsiConsole.Status()
+                              .Spinner(Spinner.Known.Binary)
+                              .StartAsync("Loading...", async ctx =>
+                              {
+                                  await Task.Delay(500);
+                              });
+
+                        var enteredFilePath = await AnsiConsole.AskAsync<string>("Enter the [green]file path[/]:");
+                        var formatedFilePath = enteredFilePath.Trim('\'', '"', '@', ' ');
+                        var result = await addressService.GetIpAddressAsync(formatedFilePath);
+                        var tableIpAddress = new Table();
+                        tableIpAddress.AddColumn("[bold white]IP Address[/]").AddColumns("[bold blue]Date[/]");
+                        foreach (KeyValuePair<string, string> kvp in result)
+                        {
+                            tableIpAddress.AddRow($"[white]{kvp.Key}[/]", $"[blue]{kvp.Value.EscapeMarkup()}[/]");
+                        }
+                        AnsiConsole.Write(tableIpAddress);
+                        break;
+                    case var t when t.Contains("Management-.Env"):
+                        AnsiConsole.MarkupLine("[green]You selected Management-.Env[/]");
+                        break;
+                    case var t when t.Contains("Net-WatchService"):
+                        AnsiConsole.MarkupLine("[green]You selected Net-WatchService[/]");
+                        break;
+                    case var t when t.Contains("Register-Dns"):
+                        AnsiConsole.MarkupLine("[green]You selected Register-Dns[/]");
+                        break;
+                    case var t when t.Contains("Sys-Auditor"):
+                        AnsiConsole.MarkupLine("[green]You selected Sys-Auditor[/]");
+                        break;
+                    case var t when t.Contains("Check-SSl/TLS"):
+                        AnsiConsole.MarkupLine("[green]You selected Check-SSl/TLS[/]");
+                        break;
+                    default:
+                        AnsiConsole.MarkupLine("[red]No valid tool selected.[/]");
+                        break;
+                }
+          
+                toppings = AnsiConsole.Prompt(
+                     new MultiSelectionPrompt<string>()
+                        .Title("Choose the [green]tool[/]...")
+                        .NotRequired()
+                        .InstructionsText("[grey](Press [blue]<space>[/] to toggle, [green]<enter>[/] to confirm)[/]")
+                        .AddChoices("Sentinela-Analyzer-Log", "Management-.Env", "Net-WatchService",
+                                    "Register-Dns", "Sys-Auditor", "Check-SSl/TLS", "[red]To finish[/]"));
+
+            }
+            AnsiConsole.MarkupLine("[red]Exiting the application...[/]");
+
+
+        }
+    }
+}
