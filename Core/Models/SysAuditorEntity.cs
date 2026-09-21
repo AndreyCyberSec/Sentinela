bb781303da5ce0d7a681328652c3df9f422543e3
Andrey-dev-hub <andreymoreira138@gmail.com>
2026-09-20 21:12:42 -0300

    foi construido a classe para ferramenta de auditoria

---
+++ Core/Models/SysAuditorEntity.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Models
+{
+    public record SysAuditorEntity
+    {
+        public string HostNameMachine { get; init; } = Environment.MachineName;
+        public string OSVersion { get; init; } = Environment.OSVersion.ToString();
+        public string CPUusage { get; init; } = string.Empty;
+        public string RAMusage { get; init; } = string.Empty;
+        public string DiskUsage { get; init; } = string.Empty;
+        public string ActiveProcesses { get; init; } = string.Empty;
+        public string NetworkInterface { get; init; } = string.Empty;
+        public string SystemUptime { get; init; } = string.Empty;
+        public string ComplianceStatus { get; init; } = string.Empty;
+        public string Message { get; init; } = string.Empty;
+        public string Timestamp { get; init; } = DateTime.UtcNow.ToString("yyyy-MM-ddTHH:mm:ssZ");
+    }
+}
